06d86c5207610bb75d179a90c98eba3a7744b045
Alfonso-Villarreal97 <dtrejov97@gmail.com>
2024-01-03 02:08:09 -0600

    Minor changes

--- Login.aspx.cs
+++ Login.aspx.cs
@@ -18,8 +18,7 @@ public class NuevoController : Page {
 	
 	protected void btnIngresar_Click(object sender, EventArgs e)
 	{		
-		Response.Redirect("Mesero/Inicio.aspx");
-	}
+		Response.Redirect("Mesero/Inicio.aspx"); //No está validando por ahora, esto se cambia
 
 
 	
