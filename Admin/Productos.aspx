56f80bbd1339cb7301bb159fe3b860e456874cd1
Vicc-v <vhugo.suarezv@gmail.com>
2024-01-07 04:56:21 -0600

    Interfaz de productos con CRUD funcional, falta modificar el diseño y agregar alertas como (Está seguro que desea eliminar?)

--- Admin/Productos.aspx
+++ Admin/Productos.aspx
@@ -1,95 +1,95 @@
 <%@ Page Language="C#" MasterPageFile="MainAdmin.master" AutoEventWireup="true" Src="Productos.aspx.cs"
     Inherits="MenusController" Title="Productos" %>
 
-    <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
-        <link rel="stylesheet" type="text/css" href="css/styles.css" />
-    </asp:Content>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <link rel="stylesheet" type="text/css" href="css/styles.css" />
+</asp:Content>
 
-    <asp:Content ID="Content2" ContentPlaceHolderID="content" runat="server">
-        <div class="productos">
-            <div class="bannerMenus">
-                <h2>Registro de alimentos y bebidas al menu del dia</h2>
+<asp:Content ID="Content2" ContentPlaceHolderID="content" runat="server">
+    <div class="productos">
+        <div class="bannerMenus">
+            <h2>REGISTRO DE ALIMENTOS AL MENU DEL DIA</h2>
+        </div>
+
+        <div class="tableMenus">
+            <div class="searchProd">
+                <label for="txtSearchProd">Filtrar por:</label>
+                <asp:DropDownList runat="server" ID="ddlCategorias" CssClass="styled-dropdown" AutoPostBack="true">
+                    <asp:ListItem Text="- Seleccionar -" Value="" />
+                </asp:DropDownList>
+                <asp:Button runat="server" ID="btnFindProd" Text="Buscar" OnClick="btnFindMenu_Click" CssClass="btnBuscar" />
+                <div class="addMenu">
+                    <a href="#openModalAdd" class="enlacesUsuarios">Agregar al menu</a>
+                </div>
             </div>
 
-            <div class="tableMenus">
-                <div class="searchProd">
-                    <label for="txtSearchProd">Buscar productos:</label>
-                    <asp:TextBox runat="server" ID="txtSearchProd" CssClass="styled-textbox"></asp:TextBox>
-                    <asp:Button runat="server" ID="btnFindProd" Text="Buscar" OnClick="btnFindMenu_Click"
-                        CssClass="btnBuscar" />                    
-                    <div class="addMenu">
-                        <a href="#openModalAdd" class="enlacesUsuarios">Agregar al menu</a>
-                    </div>                    
-                </div>    
-                
-                <div class="inputs">
-                    <asp:TextBox runat="server" ID="txtNombre" placeholder="Ingrese el nombre del producto"
-                        CssClass="styled-textbox"></asp:TextBox>
-                    <asp:TextBox runat="server" ID="txtDesc" placeholder="Ingrese la descripcion"
-                        CssClass="styled-textbox"></asp:TextBox>
-                    <asp:TextBox runat="server" ID="txtPrecio" placeholder="Ingrese el precio"
-                        CssClass="styled-textbox"></asp:TextBox>                                
-                    <label for="ddlCategoriaProd">Categoria: </label>
-                    <asp:DropDownList runat="server" ID="ddlCategoria" CssClass="styled-dropdown">
-                        <asp:ListItem Text="-" Value="Default" />
-                        <asp:ListItem Text="Tacos" Value="Tacos" />
-                        <asp:ListItem Text="Tortas" Value="Tortas" />
-                        <asp:ListItem Text="Gringas" Value="Gringas" />
-                        <asp:ListItem Text="Bebidas" Value="Bebidas" />
-                    </asp:DropDownList>
-                </div>
+            <div class="inputs">
+                <label for="ddlCategoriaProd">Categoria: </label>
+                <asp:DropDownList runat="server" ID="ddlCategoria" CssClass="styled-dropdown">
+                    <asp:ListItem Text="-" Value="Default" />
+                    <asp:ListItem Text="Tacos" Value="Tacos" />
+                    <asp:ListItem Text="Tortas" Value="Tortas" />
+                    <asp:ListItem Text="Gringas" Value="Gringas" />
+                    <asp:ListItem Text="Bebidas" Value="Bebidas" />
+                </asp:DropDownList>
 
-                <div class="buttons">
-                    <asp:Button runat="server" ID="btnUpdate" Text="Actualizar menu" 
+                <label for="txtDesc">Descripcion: </label>
+                <asp:TextBox runat="server" ID="txtDesc" placeholder="descripcion"
+                    CssClass="styled-textbox"></asp:TextBox>
+                
+                <label for="txtPrecio">Precio: </label>
+                <asp:TextBox runat="server" ID="txtPrecio" placeholder="precio"
+                    CssClass="styled-textbox"></asp:TextBox>
+            </div>
+
+            <div class="buttons">
+                <asp:Button runat="server" ID="btnUpdate" Text="Actualizar producto"
                     OnClick="btnActualizarMenu_Click" CssClass="styled-button" />
-                    <asp:Button runat="server" ID="btnDelete" Text="Eliminar del menu" 
+                <asp:Button runat="server" ID="btnDelete" Text="Eliminar producto"
                     OnClick="btnEliminarMenu_Click" CssClass="styled-button" />
-                    <asp:Button runat="server" ID="btnLimpiar" Text="Limpiar campos" 
+                <asp:Button runat="server" ID="btnLimpiar" Text="Limpiar campos"
                     OnClick="btnLimpiarMenu_Click" CssClass="styled-button" />
-                </div>
-               
-                <asp:GridView ID="GridViewProductos" runat="server" AutoGenerateColumns="False" CssClass="styled-grid"
-                 OnRowCommand="GridViewProductos_RowCommand">
+            </div>
+
+            <asp:GridView ID="GridViewProductos" runat="server" AutoGenerateColumns="False" CssClass="styled-grid"
+                OnRowCommand="GridViewProductos_RowCommand">
                 <Columns>
-                    <asp:BoundField DataField="Nombre" HeaderText="Nombre" SortExpression="Nombre" />
+                    <asp:BoundField DataField="Categoria" HeaderText="Categoria" SortExpression="Categoria" />
                     <asp:BoundField DataField="Descripcion" HeaderText="Descripcion" SortExpression="Descripcion" />
                     <asp:BoundField DataField="Precio" HeaderText="Precio" SortExpression="Precio" />
-                    <asp:BoundField DataField="Categoria" HeaderText="Categoria" SortExpression="Categoria" />                
-                   <asp:ButtonField ButtonType="Button" CommandName="Select" HeaderText="Elegir" ShowHeader="True" Text="Elegir"/>
+                    <asp:ButtonField ButtonType="Button" CommandName="Select" HeaderText="Elegir" ShowHeader="True" Text="Elegir" />
                 </Columns>
-				</asp:GridView>               
-            </div>
+            </asp:GridView>
+        </div>
 
-            <!-- ENLACES PARA ABRIR LOS MODALES CORRESPONDIENTES A LAS OPERACIONES CRUD-->
-            <div class="links">
-                <div class="modal-body">                    
-                    <div id="openModalAdd" class="modalDialog">
-                        <div>
-                            <a href="#close" title="Close" class="close">X</a>
-                            <h2>Agregar al menu</h2>
-                            <div class="inputs">
-                                <asp:TextBox runat="server" ID="txtNombreProdAlta" placeholder="Ingrese el nombre del producto"
-                                    CssClass="styled-textbox"></asp:TextBox>
-                                <asp:TextBox runat="server" ID="txtDescProdAlta" placeholder="Ingrese la descripcion"
-                                    CssClass="styled-textbox"></asp:TextBox>
-                                <asp:TextBox runat="server" ID="txtPrecioProdAlta" placeholder="Ingrese el precio"
-                                    CssClass="styled-textbox"></asp:TextBox>                                
-                                <label for="ddlCategoriaProd">Categoria: </label>
-                                <asp:DropDownList runat="server" ID="ddlCategoriaProdAlta" CssClass="styled-dropdown">
-                                    <asp:ListItem Text="-" Value="Default" />
-                                    <asp:ListItem Text="Tacos" Value="Tacos" />
-                                    <asp:ListItem Text="Tortas" Value="Tortas" />
-                                    <asp:ListItem Text="Gringas" Value="Gringas" />
-                                    <asp:ListItem Text="Bebidas" Value="Bebidas" />
-                                </asp:DropDownList>
-                            </div>
-                            <asp:Button runat="server" ID="btnAgregarMenu" Text="Agregar"
-                                OnClick="btnAgregarMenu_Click" CssClass="styled-button" />
+        <!-- ENLACES PARA ABRIR LOS MODALES CORRESPONDIENTES A LAS OPERACIONES CRUD-->
+        <div class="links">
+            <div class="modal-body">
+                <div id="openModalAdd" class="modalDialog">
+                    <div>
+                        <a href="#close" title="Close" class="close">X</a>
+                        <h2>Agregar al menu</h2>
+                        <div class="inputs">
+                            <label for="ddlCategoriaProd">Categoria: </label>
+                            <asp:DropDownList runat="server" ID="ddlCategoriaProdAlta" CssClass="styled-dropdown">
+                                <asp:ListItem Text="-" Value="Default" />
+                                <asp:ListItem Text="Tacos" Value="Tacos" />
+                                <asp:ListItem Text="Tortas" Value="Tortas" />
+                                <asp:ListItem Text="Gringas" Value="Gringas" />
+                                <asp:ListItem Text="Bebidas" Value="Bebidas" />
+                            </asp:DropDownList>
+                            <asp:TextBox runat="server" ID="txtDescProdAlta" placeholder="Ingrese la descripcion"
+                                CssClass="styled-textbox"></asp:TextBox>
+                            <asp:TextBox runat="server" ID="txtPrecioProdAlta" placeholder="Ingrese el precio"
+                                CssClass="styled-textbox"></asp:TextBox>
                         </div>
+
+                        <asp:Button runat="server" ID="btnAgregarMenu" Text="Agregar"
+                            OnClick="btnAgregarMenu_Click" CssClass="styled-button" />
                     </div>
                 </div>
-
-              
             </div>
         </div>
-    </asp:Content>
+    </div>
+</asp:Content>
+
--- Admin/Productos.aspx.cs
+++ Admin/Productos.aspx.cs
@@ -14,6 +14,7 @@ public class MenusController : Page
     public TextBox txtDescProdMod;
     public TextBox txtPrecioProdAlta;
     public TextBox txtPrecioMod;
+    public TextBox txtSearchProd;
     public DropDownList ddlCategoriaProdAlta;
     public DropDownList ddlCategoriaMod;
     public GridView GridViewProductos;
@@ -21,23 +22,27 @@ public class MenusController : Page
     public TextBox txtDesc;
     public TextBox txtPrecio;
     public DropDownList ddlCategoria;
-    
-    string connectionString = "Server=localhost;Database=bdtaque;Uid=root;Pwd=;SSLMode=preferred";
+    public DropDownList ddlCategorias;
+
+    private string selectedCategoria;
+
+    string connectionString = "Server=localhost;Database=bdtaque;Uid=root;Pwd=toor;SSLMode=preferred";
 
     protected void Page_Load(object sender, EventArgs e)
     {
         if (!IsPostBack)
         {
-			CargarDatosEnGridView();
+            CargarDatosEnGridView();
+            CargarCategoriasEnDropDownList();
         }
     }
 
-	private void CargarDatosEnGridView()
+    private void CargarDatosEnGridView()
     {
         using (MySqlConnection con = new MySqlConnection(connectionString))
         {
             con.Open();
-            string query = "SELECT ID, Nombre, Descripcion, Precio, Categoria FROM Productos";
+            string query = "SELECT ID, Categoria, Descripcion, Precio FROM Productos";
             using (MySqlCommand cmd = new MySqlCommand(query, con))
             {
                 DataTable dt = new DataTable();
@@ -51,41 +56,48 @@ public class MenusController : Page
         }
     }
 
+    private void CargarCategoriasEnDropDownList()
+	{
+		// Limpiar cualquier elemento previo en el DropDownList
+		ddlCategorias.Items.Clear();
+		
+		// Agregar un ítem por defecto
+		ddlCategorias.Items.Add(new ListItem("- TODOS -", ""));
+
+		// Añadir las categorías directamente
+		ddlCategorias.Items.Add(new ListItem("Tacos", "Tacos"));
+		ddlCategorias.Items.Add(new ListItem("Tortas", "Tortas"));
+		ddlCategorias.Items.Add(new ListItem("Gringas", "Gringas"));
+		ddlCategorias.Items.Add(new ListItem("Bebidas", "Bebidas"));
+	}
+
+
     protected void btnAgregarMenu_Click(object sender, EventArgs e)
     {
-		try
+        try
         {
-            string nombre = txtNombreProdAlta.Text;
             string descripcion = txtDescProdAlta.Text;
             decimal precio = Convert.ToDecimal(txtPrecioProdAlta.Text);
             string categoria = ddlCategoriaProdAlta.SelectedValue;
 
-            // Crear la conexión
             using (MySqlConnection con = new MySqlConnection(connectionString))
             {
                 con.Open();
 
-                // Crear el comando SQL para la inserción
-                string query = "INSERT INTO Productos (Nombre, Descripcion, Precio, Categoria) VALUES (@Nombre, @Descripcion, @Precio, @Categoria)";
+                string query = "INSERT INTO Productos (Descripcion, Precio, Categoria) VALUES (@Descripcion, @Precio, @Categoria)";
                 using (MySqlCommand cmd = new MySqlCommand(query, con))
                 {
-                    // Parametrizar los valores
-                    cmd.Parameters.AddWithValue("@Nombre", nombre);
                     cmd.Parameters.AddWithValue("@Descripcion", descripcion);
                     cmd.Parameters.AddWithValue("@Precio", precio);
                     cmd.Parameters.AddWithValue("@Categoria", categoria);
 
-                    // Ejecutar la consulta
                     cmd.ExecuteNonQuery();
-                    
-                    // Cargar datos en GridView después de la inserción
-					CargarDatosEnGridView();
+
+                    CargarDatosEnGridView();
                 }
             }
 
-            // Puedes realizar acciones adicionales después de la inserción si es necesario.
-
-            LimpiarCamposAlta(); // Método para limpiar los campos después de la inserción.
+            LimpiarCamposAlta();
         }
         catch (Exception ex)
         {
@@ -94,10 +106,8 @@ public class MenusController : Page
         }
     }
 
-	private void LimpiarCamposAlta()
+    private void LimpiarCamposAlta()
     {
-        // Método para limpiar los campos después de la inserción
-        txtNombreProdAlta.Text = string.Empty;
         txtDescProdAlta.Text = string.Empty;
         txtPrecioProdAlta.Text = string.Empty;
         ddlCategoriaProdAlta.SelectedIndex = 0;
@@ -105,8 +115,6 @@ public class MenusController : Page
 
     private void LimpiarCampos()
     {
-        // Método para limpiar los campos después de la inserción
-        txtNombre.Text = string.Empty;
         txtDesc.Text = string.Empty;
         txtPrecio.Text = string.Empty;
         ddlCategoria.SelectedIndex = 0;
@@ -114,64 +122,61 @@ public class MenusController : Page
 
     protected void btnActualizarMenu_Click(object sender, EventArgs e)
     {
-		try
-		{
-			string nombre = txtNombre.Text;
-			string descripcion = txtDesc.Text;
-			decimal precio = Convert.ToDecimal(txtPrecio.Text);
-			string categoria = ddlCategoria.SelectedValue;
-			// Actualiza el producto en la base de datos
-			using (MySqlConnection con = new MySqlConnection(connectionString))
-			{
-				con.Open();
-                //Corregir el query para que el PK asignado (En este caso es el nombre) deje de ser el PK y así hacer los cambios correctamente
-				string query = "UPDATE Productos SET Descripcion = @Descripcion, Precio = @Precio, Categoria = @Categoria WHERE Nombre = @Nombre";
-				using (MySqlCommand cmd = new MySqlCommand(query, con))
-				{
-					cmd.Parameters.AddWithValue("@Nombre", nombre);
-					cmd.Parameters.AddWithValue("@Descripcion", descripcion);
-					cmd.Parameters.AddWithValue("@Precio", precio);
-					cmd.Parameters.AddWithValue("@Categoria", categoria);
-					cmd.ExecuteNonQuery();
-				}
-			}
+        try
+        {
+            string categoria = ddlCategoria.SelectedValue;
+            string descripcion = txtDesc.Text;
+            decimal precio = Convert.ToDecimal(txtPrecio.Text);
 
-			// Vuelve a cargar los datos en el GridView después de la actualización
-			CargarDatosEnGridView();
+            using (MySqlConnection con = new MySqlConnection(connectionString))
+            {
+                con.Open();
+
+                string query = "UPDATE Productos SET Categoria = @Categoria, Descripcion = @Descripcion, Precio = @Precio  WHERE Categoria = @Categoria";
+                using (MySqlCommand cmd = new MySqlCommand(query, con))
+                {
+                    cmd.Parameters.AddWithValue("@Categoria", categoria);
+                    cmd.Parameters.AddWithValue("@Descripcion", descripcion);
+                    cmd.Parameters.AddWithValue("@Precio", precio);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            CargarDatosEnGridView();
             LimpiarCampos();
-
-	    }
-		catch (Exception ex)
-		{
-			// Manejar errores
-			// Puedes mostrar un mensaje de error o realizar acciones adicionales según sea necesario.
-		}
+        }
+        catch (Exception ex)
+        {
+            // Manejar errores
+            // Puedes mostrar un mensaje de error o realizar acciones adicionales según sea necesario.
+        }
     }
 
     protected void btnEliminarMenu_Click(object sender, EventArgs e)
     {
         try
         {
-            string nombre = txtNombre.Text;
+            string descripcion = txtDesc.Text;
 
-            // Elimina el producto de la base de datos
             using (MySqlConnection con = new MySqlConnection(connectionString))
             {
                 con.Open();
 
-                string query = "DELETE FROM Productos WHERE Nombre = @Nombre";
+                string query = "DELETE FROM Productos WHERE Descripcion = @Descripcion";
                 using (MySqlCommand cmd = new MySqlCommand(query, con))
                 {
-                    cmd.Parameters.AddWithValue("@Nombre", nombre);
+                    cmd.Parameters.AddWithValue("@Descripcion", descripcion);
                     cmd.ExecuteNonQuery();
                 }
             }
 
-            // Vuelve a cargar los datos en el GridView después de la eliminación
             CargarDatosEnGridView();
 
-            // Cierra el modal de eliminación
             ScriptManager.RegisterStartupScript(this, this.GetType(), "CloseModal", "closeModal('openModalMod');", true);
+        
+			// Limpia los campos después de eliminar
+			LimpiarCampos();
+        
         }
         catch (Exception ex)
         {
@@ -182,7 +187,42 @@ public class MenusController : Page
 
     protected void btnFindMenu_Click(object sender, EventArgs e)
     {
+        try
+        {
+            string categoriaBuscar = ddlCategorias.SelectedValue;
 
+            if (!string.IsNullOrEmpty(categoriaBuscar) && categoriaBuscar != "- Seleccionar -")
+            {
+                using (MySqlConnection con = new MySqlConnection(connectionString))
+                {
+                    con.Open();
+
+                    string query = "SELECT ID, Categoria, Descripcion, Precio FROM Productos WHERE Categoria = @Categoria";
+                    using (MySqlCommand cmd = new MySqlCommand(query, con))
+                    {
+                        cmd.Parameters.AddWithValue("@Categoria", categoriaBuscar);
+
+                        DataTable dt = new DataTable();
+                        using (MySqlDataAdapter da = new MySqlDataAdapter(cmd))
+                        {
+                            da.Fill(dt);
+                        }
+
+                        GridViewProductos.DataSource = dt;
+                        GridViewProductos.DataBind();
+                    }
+                }
+            }
+            else
+            {
+                CargarDatosEnGridView();
+            }
+        }
+        catch (Exception ex)
+        {
+            // Manejar errores
+            // Puedes mostrar un mensaje de error o realizar acciones adicionales según sea necesario.
+        }
     }
 
     protected void btnLimpiarMenu_Click(object sender, EventArgs e)
@@ -190,23 +230,21 @@ public class MenusController : Page
         LimpiarCampos();
     }
 
-   protected void GridViewProductos_RowCommand(object sender, GridViewCommandEventArgs e)
-{
-    if (e.CommandName == "Select")
+    protected void GridViewProductos_RowCommand(object sender, GridViewCommandEventArgs e)
     {
-        int rowIndex = Convert.ToInt32(e.CommandArgument);
-
-        // Asegurarse de que haya al menos una fila y la fila seleccionada sea válida
-        if (GridViewProductos.Rows.Count > 0 && rowIndex >= 0 && rowIndex < GridViewProductos.Rows.Count)
+        if (e.CommandName == "Select")
         {
-            // Obtener los valores de la fila seleccionada
-            txtNombre.Text = GridViewProductos.Rows[rowIndex].Cells[0].Text;
-            txtDesc.Text = GridViewProductos.Rows[rowIndex].Cells[1].Text;
-            txtPrecio.Text = GridViewProductos.Rows[rowIndex].Cells[2].Text;
-            ddlCategoria.SelectedValue = GridViewProductos.Rows[rowIndex].Cells[3].Text;            
+            int rowIndex = Convert.ToInt32(e.CommandArgument);
+
+            if (GridViewProductos.Rows.Count > 0 && rowIndex >= 0 && rowIndex < GridViewProductos.Rows.Count)
+            {
+                ddlCategoria.SelectedValue = GridViewProductos.Rows[rowIndex].Cells[0].Text;
+                ddlCategoria.Enabled = false;
+                txtDesc.Text = GridViewProductos.Rows[rowIndex].Cells[1].Text;
+                txtPrecio.Text = GridViewProductos.Rows[rowIndex].Cells[2].Text;
+
+                selectedCategoria = GridViewProductos.Rows[rowIndex].Cells[0].Text;
+            }
         }
     }
-}
-
-
 }
